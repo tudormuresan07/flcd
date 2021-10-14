667a44f3e1d2aa4495024dab6d5749e3f67812a7
tudormuresan07 <tudorvladmuresan@gmail.com>
2021-10-14 16:02:24 +0300

    Lab2

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        SymbolTable symbolTable=new SymbolTable();
-        System.out.println(symbolTable.insert("a"));
-        System.out.println(symbolTable.insert("a"));
-        System.out.println(symbolTable.insert("e"));
-        System.out.println(symbolTable.insert("c"));
+        SymbolTable lettersSymbolTable=new SymbolTable();
+        System.out.println(lettersSymbolTable.insert("a"));
+        System.out.println(lettersSymbolTable.insert("a"));
+        System.out.println(lettersSymbolTable.insert("e"));
+        System.out.println(lettersSymbolTable.insert("c"));
 
+        SymbolTable digitsSymbolTable=new SymbolTable();
+        System.out.println(digitsSymbolTable.insert("1"));
+        System.out.println(digitsSymbolTable.insert("3"));
+        System.out.println(digitsSymbolTable.insert("1"));
     }
 }
